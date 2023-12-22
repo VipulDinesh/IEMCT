78f0e16d7baeb2be24cf4ccbef39cd44f327a7a7
Vipul Dinesh <vipuldinesh@pm.me>
2023-12-22 22:43:32 +0530

    Problem 3

---
+++ Problem_3.py
@@ -0,0 +1,48 @@
+def Addition(a,b):
+    return a+b
+def Subtraction(a,b):
+    return a-b
+def Multiplication(a,b):
+    return a*b
+def Division(a,b):
+    return a/b
+def Modulus(a,b):
+    return a%b
+def Exponent(a,b):
+    return a**b
+
+while True:
+    print("""
+Calculator:
+
+Addition(+)
+Subtraction(-)
+Multiplication(*)
+Division(/)
+Modulus(%)
+Exponent(**)
+    """)
+
+    a=int(input("Enter number 1: "))
+    b=int(input("Enter number 2: "))
+    op=input("Enter operation[Add/Sub/Mul/Div/Mod/Exp]: ")
+
+    match op:
+        case "Add":
+            print(Addition(a,b))
+        case "Sub":
+            print(Subtraction(a,b))
+        case "Mul":
+            print(Multiplication(a,b))
+        case "Div":
+            print(Division(a,b))
+        case "Mod":
+            print(Modulus(a,b))
+        case "Exp":
+            print(Exponent(a,b))
+        case "default":
+            print("Invalid Input")
+
+    rep = input("\nRepeat?[y/n]: ")
+    if rep.lower()=="n":
+        break
